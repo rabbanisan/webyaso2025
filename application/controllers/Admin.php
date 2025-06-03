0a388abed268c21c0ccb68b06cf6d7050455f202
rabbanisan <bani.ramadhan99@gmail.com>
2025-06-03 15:03:32 +0700

    update ppdb

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,32 +1,38 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
-		public function __construct() {
-			parent::__construct();
-			$ci = get_instance();
-			if (!$ci->session->userdata('email')) {
-				redirect('Auth');
-			}
-			date_default_timezone_set("Asia/Jakarta");
+class Admin extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$ci = get_instance();
+		if (!$ci->session->userdata('email')) {
+			redirect('Auth');
 		}
-		/* the rest of your code... */
-		public function index(){
-			$tahun = DATE('Y');
-		   $data['siswa'] = $this->db->query("SELECT COUNT(*) as siswa FROM siswa WHERE tahun = $tahun")->result_array();
-		   $data['RPL'] = $this->db->query("SELECT COUNT(*) as RPL FROM siswa WHERE tahun = $tahun AND jurusan = 'RPL'")->result_array();
-		   $data['AK'] = $this->db->query("SELECT COUNT(*) as AK FROM siswa WHERE tahun = $tahun AND jurusan = 'AK'")->result_array();
-		   $data['AP'] = $this->db->query("SELECT COUNT(*) as AP FROM siswa WHERE tahun = $tahun AND jurusan = 'AP'")->result_array();
-		   $data['terima'] = $this->db->query("SELECT COUNT(*) as terima FROM siswa WHERE tahun = $tahun AND ket = 1")->result_array();
-		   $data['title'] = 'SMK Yadika Soreang';
-			$this->load->view('komponen/header1', $data);
-			$this->load->view('komponen/navbar', $data);
-			$this->load->view('Admin/index', $data);
-		}
+		date_default_timezone_set("Asia/Jakarta");
+	}
+	/* the rest of your code... */
+	public function index()
+	{
+		$tahun = DATE('Y');
+		$data['siswa'] = $this->db->query("SELECT COUNT(*) as siswa FROM siswa WHERE tahun = $tahun")->result_array();
+		$data['RPL'] = $this->db->query("SELECT COUNT(*) as RPL FROM siswa WHERE tahun = $tahun AND jurusan = 'RPL'")->result_array();
+		$data['AK'] = $this->db->query("SELECT COUNT(*) as AK FROM siswa WHERE tahun = $tahun AND jurusan = 'AK'")->result_array();
+		$data['AP'] = $this->db->query("SELECT COUNT(*) as AP FROM siswa WHERE tahun = $tahun AND jurusan = 'AP'")->result_array();
+		$data['RPL_terima'] = $this->db->query("SELECT COUNT(*) as RPL FROM siswa WHERE tahun = $tahun AND ket = 1 AND jurusan = 'RPL'")->result_array();
+		$data['AK_terima'] = $this->db->query("SELECT COUNT(*) as AK FROM siswa WHERE tahun = $tahun AND ket = 1 AND jurusan = 'AK'")->result_array();
+		$data['AP_terima'] = $this->db->query("SELECT COUNT(*) as AP FROM siswa WHERE tahun = $tahun AND ket = 1 AND jurusan = 'AP'")->result_array();
+		$data['terima'] = $this->db->query("SELECT COUNT(*) as terima FROM siswa WHERE tahun = $tahun AND ket = 1")->result_array();
+		$data['title'] = 'SMK Yadika Soreang';
+		$this->load->view('komponen/header1', $data);
+		$this->load->view('komponen/navbar', $data);
+		$this->load->view('Admin/index', $data);
+	}
 	public function data()
 	{
 		$tahun = DATE('Y');
-       $data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', ['tahun'=>$tahun]);
+		$data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', ['tahun' => $tahun]);
 		$data['title'] = 'SMK Yadika Soreang';
 		$this->load->view('komponen/header', $data);
 		$this->load->view('komponen/navbar', $data);
@@ -36,7 +42,7 @@ class Admin extends CI_Controller {
 	public function rpl()
 	{
 		$tahun = DATE('Y');
-		$array = array('jurusan'=>'RPL', 'tahun'=>$tahun);
+		$array = array('jurusan' => 'RPL', 'tahun' => $tahun);
 		$data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', $array);
 		$data['title'] = 'SMK Yadika Soreang';
 		$this->load->view('komponen/header', $data);
@@ -47,7 +53,7 @@ class Admin extends CI_Controller {
 	public function AK()
 	{
 		$tahun = DATE('Y');
-		$array = array('jurusan'=>'AK', 'tahun'=>$tahun);
+		$array = array('jurusan' => 'AK', 'tahun' => $tahun);
 		$data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', $array);
 		$data['title'] = 'SMK Yadika Soreang';
 		$this->load->view('komponen/header', $data);
@@ -58,7 +64,7 @@ class Admin extends CI_Controller {
 	public function AP()
 	{
 		$tahun = DATE('Y');
-		$array = array('jurusan'=>'AP', 'tahun'=>$tahun);
+		$array = array('jurusan' => 'AP', 'tahun' => $tahun);
 		$data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', $array);
 		$data['title'] = 'SMK Yadika Soreang';
 		$this->load->view('komponen/header', $data);
@@ -66,10 +72,11 @@ class Admin extends CI_Controller {
 		$this->load->view('Admin/jurusan/AP', $data);
 		//$this->load->view('komponen/footer', $data);
 	}
-	public function download(){
- // Create new Spreadsheet object
+	public function download()
+	{
+		// Create new Spreadsheet object
 
-	 }
+	}
 	// public function tambah_data()
 	// {
 	// 	$agama = "";
@@ -106,14 +113,16 @@ class Admin extends CI_Controller {
 	// 	];
 	// 	$this->db->insert('siswa',$data);
 	// }
-	public function hapus($id){
-		$where = ['id_siswa'=>$id];
-		$this->db->delete('siswa',$where);
+	public function hapus($id)
+	{
+		$where = ['id_siswa' => $id];
+		$this->db->delete('siswa', $where);
 		redirect('Admin');
 	}
-	public function terima($id){
+	public function terima($id)
+	{
 		$data = array(
-			'ket'=>1,
+			'ket' => 1,
 		);
 		$where = array(
 			'id_siswa' => $id,
@@ -121,9 +130,10 @@ class Admin extends CI_Controller {
 		$this->db->update('siswa', $data, $where);
 		redirect('Admin/data');
 	}
-	public function wawancaras($id){
+	public function wawancaras($id)
+	{
 		$data = array(
-			'wawancara'=>1,
+			'wawancara' => 1,
 		);
 		$where = array(
 			'id_siswa' => $id,
@@ -131,9 +141,10 @@ class Admin extends CI_Controller {
 		$this->db->update('siswa', $data, $where);
 		redirect('Terima');
 	}
-	public function no_terima($id){
+	public function no_terima($id)
+	{
 		$data = array(
-			'ket'=>2,
+			'ket' => 2,
 		);
 		$where = array(
 			'id_siswa' => $id,
@@ -141,40 +152,43 @@ class Admin extends CI_Controller {
 		$this->db->update('siswa', $data, $where);
 		redirect('Admin/data');
 	}
-	public function update_nominal(){
+	public function update_nominal()
+	{
 		$nominal = $this->input->post('new_value');
-		$status = ($nominal >= 1500000) ? 1 : 0;
-			$data = array(
-				'nominal'=>$nominal,
-				'ket'=>$status
-			);
+		$status = ($nominal >= 2000000) ? 1 : 0;
+		$data = array(
+			'nominal' => $nominal,
+			'ket' => $status
+		);
 		$where = array(
 			'id_siswa' => $this->input->post('id'),
 		);
 		$this->db->update('siswa', $data, $where);
 		redirect('Admin/data');
 	}
-	public function edit($id){
+	public function edit($id)
+	{
 		$where = array(
 			'id_siswa' => $id,
 		);
-		$this->form_validation->set_rules('Nama', 'Nama', 'required|max_length[25]|regex_match[/^[A-Za-z0-9\s]+$/]',[ 'max_lenght'=>'maximal 25 huruf', 'required'=>'Nama Tidak Boleh Kosong', 'regex_match'=>'tidak boleh mengandung simbol']);
-		$this->form_validation->set_rules('namaOrtu', 'namaOrtu', 'required|max_length[25]|regex_match[/^[A-Za-z0-9\s]+$/]',['max_length'=>'maximal 25 huruf', 'required'=>'Nama Orang Tua Tidak Boleh Kosong','regex_match'=>'tidak boleh mengandung simbol']);
-		$this->form_validation->set_rules('asek', 'asek', 'required|max_length[25]|regex_match[/^[A-Za-z0-9\s]+$/]',['max_length'=>'maximal 25 huruf', 'required'=>'Asal Sekolah Tidak Boleh Kosong','regex_match'=>'tidak boleh mengandung simbol']);
-		$this->form_validation->set_rules('no_hp', 'no_hp', 'required|max_length[13]|regex_match[/^[A-Za-z0-9\s]+$/]',['max_length'=>'maximal 13 huruf', 'required'=>'Nomer Telepon Tidak Boleh Kosong','regex_match'=>'tidak boleh mengandung simbol']);
-		$this->form_validation->set_rules('alamat', 'alamat', 'required|regex_match[/^[A-Za-z0-9\s]+$/]',['required'=>'Alamat Tidak Boleh Kosong','regex_match'=>'tidak boleh mengandung simbol']);
+		$this->form_validation->set_rules('Nama', 'Nama', 'required|max_length[25]|regex_match[/^[A-Za-z0-9\s]+$/]', ['max_lenght' => 'maximal 25 huruf', 'required' => 'Nama Tidak Boleh Kosong', 'regex_match' => 'tidak boleh mengandung simbol']);
+		$this->form_validation->set_rules('namaOrtu', 'namaOrtu', 'required|max_length[25]|regex_match[/^[A-Za-z0-9\s]+$/]', ['max_length' => 'maximal 25 huruf', 'required' => 'Nama Orang Tua Tidak Boleh Kosong', 'regex_match' => 'tidak boleh mengandung simbol']);
+		$this->form_validation->set_rules('asek', 'asek', 'required|max_length[25]|regex_match[/^[A-Za-z0-9\s]+$/]', ['max_length' => 'maximal 25 huruf', 'required' => 'Asal Sekolah Tidak Boleh Kosong', 'regex_match' => 'tidak boleh mengandung simbol']);
+		$this->form_validation->set_rules('no_hp', 'no_hp', 'required|max_length[13]|regex_match[/^[A-Za-z0-9\s]+$/]', ['max_length' => 'maximal 13 huruf', 'required' => 'Nomer Telepon Tidak Boleh Kosong', 'regex_match' => 'tidak boleh mengandung simbol']);
+		$this->form_validation->set_rules('alamat', 'alamat', 'required|regex_match[/^[A-Za-z0-9\s]+$/]', ['required' => 'Alamat Tidak Boleh Kosong', 'regex_match' => 'tidak boleh mengandung simbol']);
 		if ($this->form_validation->run() == FALSE) {
-		$data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', $where);
-		$data['title'] = 'PPDB SMK Yadika Soreang';
-		$this->load->view('PPDB/header', $data);
+			$data['siswa'] = $this->Ppdb_m->sesuaikan('siswa', $where);
+			$data['title'] = 'PPDB SMK Yadika Soreang';
+			$this->load->view('PPDB/header', $data);
 			$this->load->view('Admin/edit', $data);
-		}else{
+		} else {
 			update();
 		}
 	}
-		public function update(){
+	public function update()
+	{
 		$id = $this->input->post('id');
-			$data = [
+		$data = [
 			'Nama' => htmlspecialchars($this->input->post('Nama')),
 			'alamat' => htmlspecialchars($this->input->post('alamat')),
 			'nama_ortu' => htmlspecialchars($this->input->post('namaOrtu')),
@@ -184,12 +198,12 @@ class Admin extends CI_Controller {
 			'agama' => $this->input->post('agama'),
 			'jurusan' => $this->input->post('jurusan'),
 			'nominal' => $this->input->post('nominal'),
-			'tahun'=>DATE('Y'),
-			'ket'=>1
+			'tahun' => DATE('Y'),
+			'ket' => 1
 		];
-		$where = array('id_siswa'=>$id);
+		$where = array('id_siswa' => $id);
 		$this->session->set_flashdata('success_message', 'Data siswa berhasil ditambahkan.');
-		$this->db->update('siswa',$data,$where);
-		redirect('Admin/data');	
-		}
+		$this->db->update('siswa', $data, $where);
+		redirect('Admin/data');
 	}
+}
--- application/views/Admin/data_siswa.php
+++ application/views/Admin/data_siswa.php
@@ -1,113 +1,133 @@
     <style>
-         th:nth-child(6) {
-      width: 12%; /* Atur lebar kolom Nominal menjadi 20% */
-    }
+        th:nth-child(6) {
+            width: 12%;
+            /* Atur lebar kolom Nominal menjadi 20% */
+        }
     </style>
-<body>
-<table id="datatablesSimple" class="table">
-    <thead>
-        <tr>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Nama Orang Tua</th>
-            <th>Asal Sekolah</th>
-            <th>Nominal</th>
-            <th>Jurusan</th>
-            <th>Keterangan</th>
-            <th>status</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($siswa as $a):?>
-            <tr>
-            <td>
-                <?= $a['Nama']; ?>
-            </td>
-            <td>
-                <?= $a['alamat']; ?>
-            </td>
-            <td>
-                <?= $a['nama_ortu']; ?>
-            </td>
-            <td>
-                <?= $a['asek']; ?>
-            </td>
-            <td style="padding: 8px;
+
+    <body>
+        <table id="datatablesSimple" class="table">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                    <th>Nama Orang Tua</th>
+                    <th>Asal Sekolah</th>
+                    <th>Nominal</th>
+                    <th>Jurusan</th>
+                    <th>Keterangan</th>
+                    <th>status</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($siswa as $a): ?>
+                    <tr>
+                        <td>
+                            <?= $a['Nama']; ?>
+                        </td>
+                        <td>
+                            <?= $a['alamat']; ?>
+                        </td>
+                        <td>
+                            <?= $a['nama_ortu']; ?>
+                        </td>
+                        <td>
+                            <?= $a['asek']; ?>
+                        </td>
+                        <td style="padding: 8px;
 ">
-                <?php if($a['nominal'] == 0 ){?>
-                    <a href="#" class="badge bg-primary" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?= $a['id_siswa']; ?>">tambah</a>
-                    <?php }else{
-                        echo  $rupiah = "Rp " . number_format($a['nominal'], 0, ',', '.');
-                    } ?>
-            </td>
-            <td>
-            <?php if($a['jurusan']=='AP'){
-                    echo "Perhotelan";
-                }else{?>
-                <?= $a['jurusan'];} ?>
-            </td>
-            <td>
-                <?php if($a['ket']==0){
-                    echo anchor('Admin/terima/'. $a['id_siswa'], '<div class="badge bg-info" onclick="return confirm(\'apakah anda yakin ingin menerima siswa ini?\')">terima</div>');
-                    echo anchor('Admin/no_terima/'. $a['id_siswa'], '<div class="badge bg-danger" onclick="return confirm(\'apakah anda yakin ingin menerima siswa ini?\')">tolak</div>');
-                }else if($a['ket']==2){
-                    echo anchor('Admin/terima/'. $a['id_siswa'], '<div class="badge bg-info" onclick="return confirm(\'apakah anda yakin ingin menerima siswa ini?\')">terima</div>');
-                }?>
-                    </td>
-                <td>
+                            <?php
+                            if ($a['nominal'] == 0) { ?>
+                                <a href="#" class="badge bg-primary" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?= $a['id_siswa']; ?>">tambah</a>
+                            <?php
+                            } elseif ($a['nominal'] < 2000000) {
+                                echo "Rp " . number_format($a['nominal'], 0, ',', '.'); ?>
+                                <a href="#" class="badge bg-primary" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?= $a['id_siswa']; ?>">tambah</a>
+                            <?php
+                            } else {
+                                echo "Rp " . number_format($a['nominal'], 0, ',', '.');
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <?php if ($a['jurusan'] == 'AP') {
+                                echo "Perhotelan";
+                            } else { ?>
+                            <?= $a['jurusan'];
+                            } ?>
+                        </td>
+                        <td>
+                            <?php if ($a['ket'] == 0) {
+                                echo anchor('Admin/terima/' . $a['id_siswa'], '<div class="badge bg-info" onclick="return confirm(\'apakah anda yakin ingin menerima siswa ini?\')">terima</div>');
+                                echo anchor('Admin/no_terima/' . $a['id_siswa'], '<div class="badge bg-danger" onclick="return confirm(\'apakah anda yakin ingin menerima siswa ini?\')">tolak</div>');
+                            } else if ($a['ket'] == 2) {
+                                echo anchor('Admin/terima/' . $a['id_siswa'], '<div class="badge bg-info" onclick="return confirm(\'apakah anda yakin ingin menerima siswa ini?\')">terima</div>');
+                            } ?>
+                        </td>
+                        <td>
+                            <?php
+                            echo anchor('Admin/hapus/' . $a['id_siswa'], '<div class="badge bg-danger" onclick="return confirm(\'apakah anda yakin ingin menghapus ini?\')">hapus</div>');
+                            ?>
+                            <?php
+                            echo anchor('Admin/edit/' . $a['id_siswa'], '<div class="badge bg-warning" onclick="return confirm(\'apakah anda yakin ingin mengedit ini?\')">edit</div>');
+                            ?>
+                        </td>
+                        <td>
 
-                    <?php
-                if($a['ket']==0){
-                    ?>
-                <p style="color:grey">Proses</p>
-                <?php }else if($a['ket']==1){?>
-                    <p style="color:green">diterima</p>
-                    <?php }else{?>
-                        <p style="color:red">tidak diterima</p>
-                    <?php } ?>
-            </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+                            <?php
+                            if ($a['ket'] == 0) {
+                            ?>
+                                <p style="color:grey">Proses</p>
+                            <?php } else if ($a['ket'] == 1) { ?>
+                                <p style="color:green">diterima</p>
+                            <?php } else { ?>
+                                <p style="color:red">tidak diterima</p>
+                            <?php } ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
-    <a href="<?= base_url('Excel') ?>"><button class="badge bg-info">Download</button></a>
-    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Masukan Jumlah Nominal</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form action="<?php echo base_url('Admin/update_nominal'); ?>" method="POST">
-        <input type="hidden" name="id" id="editId">
-          <label for="value">Value:</label>
-          <input type="text" name="new_value" value="" class="form-control">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-success">Save</button>
-      </div>
-        </form>
-    </div>
-  </div>
-</div>
+        <a href="<?= base_url('Excel') ?>"><button class="badge bg-info">Download</button></a>
+        <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Masukan Jumlah Nominal</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="<?php echo base_url('Admin/update_nominal'); ?>" method="POST">
+                            <input type="hidden" name="id" id="editId">
+                            <label for="value">Value:</label>
+                            <input type="text" name="new_value" value="" class="form-control">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-success">Save</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-<!-- CSS Bootstrap -->
+        <!-- CSS Bootstrap -->
 
-<!-- JavaScript Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-<script src="<?= base_url() ?>Assets/js/datatables-simple-demo.js"></script>
-<script>
-  $(document).ready(function() {
-    $('#editModal').on('show.bs.modal', function(event) {
-      var button = $(event.relatedTarget);
-      var id = button.data('id');
-      $('#editId').val(id);
-    });
-  });
-</script>
-</body>
-</html>
+        <!-- JavaScript Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="<?= base_url() ?>Assets/js/datatables-simple-demo.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#editModal').on('show.bs.modal', function(event) {
+                    var button = $(event.relatedTarget);
+                    var id = button.data('id');
+                    $('#editId').val(id);
+                });
+            });
+        </script>
+    </body>
+
+    </html>
